7148143587eb35b837549ec25ffa04fede672ca8
sergeyesp <sergey_esp@i.ua>
2023-10-14 20:38:32 +0300

    Lesson3.1

--- app/src/main/java/com/example/roomtest/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/roomtest/adapter/NoteAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.roomtest.adapter
 
-class NoteAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomtest.model.NoteModel
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var listNote = emptyList<NoteModel>()
+    class NoteViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+
+    }
 }
