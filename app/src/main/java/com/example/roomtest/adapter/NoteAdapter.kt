07630dbcc099ab6e088bc070c95c2b6144ab126b
sergeyesp <sergey_esp@i.ua>
2023-10-15 12:47:58 +0300

    Lesson3.2

--- app/src/main/java/com/example/roomtest/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/roomtest/adapter/NoteAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.roomtest.adapter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roomtest.R
 import com.example.roomtest.model.NoteModel
+import kotlinx.android.synthetic.main.item_layout.view.*
 
 class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
@@ -11,14 +15,21 @@ class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     class NoteViewHolder(view: View): RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return NoteViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-
+        return listNote.size
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.itemView.item_title.text = listNote[position].title
+    }
 
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(list: List<NoteModel>) {
+        listNote = list
+        notifyDataSetChanged()
     }
 }
