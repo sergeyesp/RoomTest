c63ecb9c2b3897659c0a785935146201d1202ca6
sergeyesp <sergey_esp@i.ua>
2023-10-15 12:53:48 +0300

    Lesson4.1

--- app/src/main/java/com/example/roomtest/db/repository/NoteRepository.kt
+++ app/src/main/java/com/example/roomtest/db/repository/NoteRepository.kt
@@ -1,4 +1,10 @@
 package com.example.roomtest.db.repository
 
+import androidx.lifecycle.LiveData
+import com.example.roomtest.model.NoteModel
+
 interface NoteRepository {
+    val allNotes: LiveData<List<NoteModel>>
+    suspend fun insertNote(noteModel: NoteModel, onSuccess:() -> Unit)
+    suspend fun deleteNote(noteModel: NoteModel, onSuccess:() -> Unit)
 }
