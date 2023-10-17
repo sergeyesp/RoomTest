8db63cc1a750b3b4de36686b7f2ccd93e668ccd5
sergeyesp <sergey_esp@i.ua>
2023-10-17 11:00:14 +0300

    Lesson4.3

--- app/src/main/java/com/example/roomtest/db/NoteDataBase.kt
+++ app/src/main/java/com/example/roomtest/db/NoteDataBase.kt
@@ -1,10 +1,27 @@
 package com.example.roomtest.db
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.roomtest.db.dao.NoteDao
 import com.example.roomtest.model.NoteModel
 
 @Database(entities = [NoteModel::class], version = 1)
 abstract class NoteDataBase: RoomDatabase() {
+    abstract fun getNoteDao(): NoteDao
+
+    companion object {
+        private var database: NoteDataBase ?= null
+
+        @Synchronized
+        fun getInstance(context: Context): NoteDataBase {
+            return if (database == null) {
+                database = Room.databaseBuilder(context, NoteDataBase::class.java,"db").build()
+                database as NoteDataBase
+            } else {
+                database as NoteDataBase
+            }
+        }
+    }
 }
