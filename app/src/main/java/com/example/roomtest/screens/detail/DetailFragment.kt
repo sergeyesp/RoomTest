22f550f43abfd055c2640fd430451dcad5bfe9ea
sergeyesp <sergey_esp@i.ua>
2023-10-26 11:08:28 +0300

    Lesson6.5

--- app/src/main/java/com/example/roomtest/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/example/roomtest/screens/detail/DetailFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.roomtest.APP
+import com.example.roomtest.MainActivity
 import com.example.roomtest.R
 import com.example.roomtest.databinding.FragmentDetailBinding
 import com.example.roomtest.model.NoteModel
@@ -29,11 +32,16 @@ class DetailFragment : Fragment() {
     }
 
     private fun init() {
+        val viewModel = ViewModelProvider(this)[DetailViewModel::class.java]
         binding.tvTitleDetail.text = currentNote.title
         binding.tvDescDetail.text = currentNote.description
 
+        binding.btnDelete.setOnClickListener {
+            viewModel.delete(currentNote){}
+            APP.navController.navigate(R.id.action_detailFragment_to_startFragment)
+        }
         binding.btnBack.setOnClickListener {
-
+            APP.navController.navigate(R.id.action_detailFragment_to_startFragment)
         }
     }
 }
--- app/src/main/java/com/example/roomtest/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/roomtest/screens/detail/DetailViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.roomtest.screens.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomtest.REPOSITORY
+import com.example.roomtest.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailViewModel : ViewModel() {
+
+    fun delete(noteModel: NoteModel, onSuccess: () -> Unit) =
+        viewModelScope.launch(Dispatchers.IO) {
+            REPOSITORY.deleteNote(noteModel) {
+                onSuccess()
+            }
+        }
 }
